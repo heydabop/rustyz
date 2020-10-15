57bd745fef86b871b7db5e3e42a5cdf7605e1473
heydabop <heydabop@gmail.com>
2020-10-14 22:23:38 -0500

    crop transparent png and send that if possible for wow images

--- src/commands/wow.rs
+++ src/commands/wow.rs
@@ -1,9 +1,12 @@
 use crate::{WowAuth, WowConfig};
+use image::{imageops, png::PngEncoder, ColorType, ImageFormat};
 use reqwest::{StatusCode, Url};
 use serde::Deserialize;
 use serenity::client::Context;
 use serenity::framework::standard::{macros::command, Args, CommandError, CommandResult};
+use serenity::http::AttachmentType;
 use serenity::model::channel::Message;
+use std::borrow::Cow;
 use std::time::{Duration, SystemTime};
 
 #[derive(Deserialize)]
@@ -12,13 +15,13 @@ struct AuthResponse {
     expires_in: u64,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct KeyValue {
     key: String,
     value: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct CharacterMedia {
     assets: Option<Vec<KeyValue>>,
     render_url: Option<String>,
@@ -88,19 +91,97 @@ pub async fn character(ctx: &Context, msg: &Message, mut args: Args) -> CommandR
             Err(e) => return Err(CommandError::from(e))
         };
 
-    let mut image_url = String::from("");
-    if let Some(url) = resp.render_url {
-        image_url = url;
-    } else {
-        for entry in resp.assets.unwrap() {
-            if entry.key == "main" {
-                image_url = entry.value;
-                break;
+    let mut found_raw = false;
+    let mut image_url = None;
+    if let Some(assets) = resp.assets {
+        if let Some(raw) = assets.iter().find(|&a| a.key == "main-raw") {
+            found_raw = true;
+            image_url = Some(raw.value.clone());
+        } else if let Some(main) = assets.iter().find(|&a| a.key == "main") {
+            image_url = Some(main.value.clone());
+        }
+    }
+    if image_url.is_none() {
+        image_url = resp.render_url;
+    }
+    if image_url.is_none() {
+        return Err(CommandError::from("Unable to find character imagery"));
+    }
+    if !found_raw {
+        msg.channel_id.say(&ctx.http, image_url.unwrap()).await?;
+        return Ok(());
+    }
+
+    let image_bytes = reqwest::get(Url::parse(&image_url.unwrap()).unwrap())
+        .await?
+        .bytes()
+        .await?;
+    let mut image =
+        image::load_from_memory_with_format(&image_bytes, ImageFormat::Png)?.into_rgba();
+    let (width, height) = image.dimensions();
+
+    let mut top = 0;
+    let mut bottom = height;
+    let mut left = 0;
+    let mut right = width;
+
+    // find top most non-blank pixel
+    'top: for y in 0..height {
+        for x in 0..width {
+            if image[(x, y)][3] > 0 {
+                // check alpha channel for non-transparency
+                top = y;
+                break 'top;
+            }
+        }
+    }
+    // find bottom most non-blank pixel
+    'bottom: for y in (top..height).rev() {
+        for x in 0..width {
+            if image[(x, y)][3] > 0 {
+                bottom = (y + 1).min(height);
+                break 'bottom;
+            }
+        }
+    }
+    // find left most non-blank pixel
+    'left: for x in 0..width {
+        for y in top..bottom {
+            if image[(x, y)][3] > 0 {
+                left = x;
+                break 'left;
+            }
+        }
+    }
+    // find right most non-blank pixel
+    'right: for x in (left..width).rev() {
+        for y in top..bottom {
+            if image[(x, y)][3] > 0 {
+                right = (x + 1).min(width);
+                break 'right;
             }
         }
     }
 
-    msg.channel_id.say(&ctx.http, image_url).await?;
+    let cropped_image =
+        imageops::crop(&mut image, left, top, right - left, bottom - top).to_image();
+    let mut cropped_buffer = Vec::new();
+    PngEncoder::new(&mut cropped_buffer).encode(
+        &cropped_image,
+        right - left,
+        bottom - top,
+        ColorType::Rgba8,
+    )?;
+
+    msg.channel_id
+        .send_message(&ctx.http, |m| {
+            m.add_file(AttachmentType::Bytes {
+                data: Cow::from(cropped_buffer),
+                filename: format!("{}.png", arg),
+            });
+            m
+        })
+        .await?;
 
     Ok(())
 }
