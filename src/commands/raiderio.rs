1851e17a3629729ea7d0bcb3438a9d981794e933
heydabop <heydabop@gmail.com>
2020-12-26 12:06:06 -0600

    stop sending empty strings to discord embed

--- src/commands/raiderio.rs
+++ src/commands/raiderio.rs
@@ -9,7 +9,7 @@ use std::time::SystemTime;
 const PLUSSES: [&str; 4] = ["", "+", "++", "+++"];
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct CharacterProfile {
     name: String,
     race: String,
@@ -26,14 +26,14 @@ struct CharacterProfile {
 }
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct MythicPlusSeasonScores {
     season: String,
     scores: MythicPlusScores,
 }
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct MythicPlusScores {
     all: f32,
     dps: f32,
@@ -42,7 +42,7 @@ struct MythicPlusScores {
 }
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct MythicPlusRun {
     dungeon: String,
     short_name: String,
@@ -55,14 +55,14 @@ struct MythicPlusRun {
 }
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct RaidProgression {
     #[serde(rename = "castle-nathria")]
     castle_nathria: Raid,
 }
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Raid {
     summary: String,
     total_bosses: u8,
@@ -71,13 +71,13 @@ struct Raid {
     mythic_bosses_killed: u8,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct StaticData {
     dungeons: Vec<Dungeon>,
 }
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Dungeon {
     id: u32,
     short_name: String,
@@ -135,23 +135,6 @@ pub async fn raiderio(ctx: &Context, msg: &Message, args: Args) -> CommandResult
         }
     };
 
-    let mut best_runs_by_dungeon: HashMap<String, Option<&MythicPlusRun>> = HashMap::new();
-    let mut num_dungeons = 0;
-    let mut longest_name: usize = 0;
-    for run in &profile.mythic_plus_best_runs {
-        if num_dungeons == dungeons.len() {
-            break;
-        }
-        if let Some(run) = best_runs_by_dungeon.get(&run.short_name) {
-            if run.is_some() {
-                continue;
-            }
-        }
-        best_runs_by_dungeon.insert(run.short_name.clone(), Some(run));
-        num_dungeons += 1;
-        longest_name = run.short_name.len().max(longest_name);
-    }
-
     let thumbnail_url = if let Ok(t) = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
         format!("{}?{}", profile.thumbnail_url, t.as_secs())
     } else {
@@ -160,22 +143,45 @@ pub async fn raiderio(ctx: &Context, msg: &Message, args: Args) -> CommandResult
 
     let highest_runs = format_runs(&profile.mythic_plus_highest_level_runs, 5);
     let recent_runs = format_runs(&profile.mythic_plus_recent_runs, 5);
-    let mut sorted_best_runs = Vec::with_capacity(num_dungeons);
-    for (short_name, run) in best_runs_by_dungeon {
-        if let Some(run) = run {
-            sorted_best_runs.push(format!(
-                "`{:width$}` {}{}",
-                short_name,
-                run.mythic_level,
-                PLUSSES[run.num_keystone_upgrades as usize],
-                width = longest_name
-            ));
-        } else {
-            sorted_best_runs.push(format!("`{:width$}` --", short_name, width = longest_name));
+
+    let best_runs = if profile.mythic_plus_best_runs.is_empty() {
+        String::from("No runs")
+    } else {
+        let mut best_runs_by_dungeon: HashMap<String, Option<&MythicPlusRun>> = HashMap::new();
+        let mut num_dungeons = 0;
+        let mut longest_name: usize = 0;
+        for run in &profile.mythic_plus_best_runs {
+            if num_dungeons == dungeons.len() {
+                break;
+            }
+            if let Some(run) = best_runs_by_dungeon.get(&run.short_name) {
+                if run.is_some() {
+                    continue;
+                }
+            }
+            best_runs_by_dungeon.insert(run.short_name.clone(), Some(run));
+            num_dungeons += 1;
+            longest_name = run.short_name.len().max(longest_name);
         }
-    }
-    sorted_best_runs.sort();
-    let best_runs = sorted_best_runs.join("\n");
+
+        let mut sorted_best_runs = Vec::with_capacity(num_dungeons);
+        for (short_name, run) in best_runs_by_dungeon {
+            if let Some(run) = run {
+                sorted_best_runs.push(format!(
+                    "`{:width$}` {}{}",
+                    short_name,
+                    run.mythic_level,
+                    PLUSSES[run.num_keystone_upgrades as usize],
+                    width = longest_name
+                ));
+            } else {
+                sorted_best_runs.push(format!("`{:width$}` --", short_name, width = longest_name));
+            }
+        }
+        sorted_best_runs.sort();
+
+        sorted_best_runs.join("\n")
+    };
 
     msg.channel_id
         .send_message(&ctx.http, |m| {
@@ -201,6 +207,9 @@ pub async fn raiderio(ctx: &Context, msg: &Message, args: Args) -> CommandResult
 }
 
 fn format_runs(runs: &[MythicPlusRun], count: usize) -> String {
+    if runs.is_empty() {
+        return String::from("No runs");
+    }
     let mut s = String::with_capacity(5 * count);
     for run in runs.iter().take(count) {
         s.push_str(&format!(
